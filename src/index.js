a8632c1e5a6dc62ad116433173c63161ec569712
Ashell Fernando <ashellz2010@gmail.com>
2020-08-31 20:06:27 -0400

    chore: refactor env constants

--- src/index.js
+++ src/index.js
@@ -1,48 +1,48 @@
-require('dotenv').config();
-const { Client, WebhookClient } = require('discord.js');
-const { Greetings, Commands, Functions } = require('./util/helper');
+require("dotenv").config();
+const { Client, WebhookClient } = require("discord.js");
+const { Greetings, Commands, Functions, Constants } = require("./util/helper");
 
 // discord.js uses cached set of data,
 // beginning from when the bot was last online.
 // use partials to access data that isn't cached.
 const client = new Client({
-    partials: ['MESSAGE', 'REACTION'],
+	partials: ["MESSAGE", "REACTION"],
 });
 const webhookClient = new WebhookClient(
-    process.env.WEBHOOK_ID,
-    process.env.WEBHOOK_TOKEN
+	Constants.env.GITHUB_WEBHOOK_ID,
+	Constants.env.GITHUB_WEBHOOK_TOKEN
 );
-const PREFIX = '%';
+const PREFIX = "%";
 
 // login
-client.login(process.env.BOT_TOKEN);
-client.on('ready', () => {
-    console.log(`${client.user.tag} is ready`);
+client.login(Constants.env.BOT_TOKEN);
+client.on("ready", () => {
+	console.log(`${client.user.tag} is ready`);
 });
 
 // message event listener
-client.on('message', (message) => {
-    const CONTENT = message.content;
+client.on("message", (message) => {
+	const CONTENT = message.content;
 
-    console.log(`[${message.author.tag}: ${CONTENT}]`);
+	console.log(`[${message.author.tag}: ${CONTENT}]`);
 
-    // mitigating counting bot's own reply
-    // to avoid a loop
-    if (message.author.bot) return;
+	// mitigating counting bot's own reply
+	// to avoid a loop
+	if (message.author.bot) return;
 
-    // checking against our list of greetings
-    if (Greetings.includes(CONTENT)) {
-        message.channel.send(`Hello there!`);
-    }
+	// checking against our list of greetings
+	if (Greetings.includes(CONTENT)) {
+		message.reply(`Hello there!`);
+	}
 
-    // commands
-    if (CONTENT.startsWith(PREFIX)) {
-        const [CMD_NAME, ...args] = CONTENT.trim()
-            .substring(PREFIX.length)
-            .split(/\s+/);
+	// commands
+	if (CONTENT.startsWith(PREFIX)) {
+		const [CMD_NAME, ...args] = CONTENT.trim()
+			.substring(PREFIX.length)
+			.split(/\s+/);
 
-        console.log(message.author.tag, 'used command:', CMD_NAME, args);
-        /* TODO
+		console.log(message.author.tag, "used command:", CMD_NAME, args);
+		/* TODO
         handle channel vs DM
          - the bot gets events from channel and DM all the same.
          - DM doesn't have channel methods
@@ -50,84 +50,82 @@ client.on('message', (message) => {
          - currently the bot would crash if someone were to DM a command
           - because 'member' is searching cached server user listing
          */
-        if (args.length > 0) {
-            // double checking the commands
-            if (Commands.includes(CMD_NAME)) {
-                if (message.channel.type == 'text') {
-                    const MEMBER = message.guild.members.cache.get(args[0]);
-                    switch (CMD_NAME) {
-                        case 'kick':
-                            Functions.checkKickable(message, MEMBER);
-                            break;
-                        case 'ban':
-                            Functions.checkBannable(message, MEMBER);
-                            break;
-                        case 'say':
-                            message.channel.send(args.join(' '));
-                            break;
-                        case 'announce':
-                            webhookClient.send(args.join(' '));
-                            break;
-                        default:
-                            console.log(
-                                'command received, could not parse command'
-                            );
-                            return;
-                    }
-                }
-            } else {
-                message.reply(
-                    "can't follow through homie, didn't catch that last part"
-                );
-            }
-        }
-    }
+		if (args.length > 0) {
+			// double checking the commands
+			if (Commands.includes(CMD_NAME)) {
+				if (message.channel.type == "text") {
+					const MEMBER = message.guild.members.cache.get(args[0]);
+					switch (CMD_NAME) {
+						case "kick":
+							Functions.checkKickable(message, MEMBER);
+							break;
+						case "ban":
+							Functions.checkBannable(message, MEMBER);
+							break;
+						case "say":
+							message.channel.send(args.join(" "));
+							break;
+						case "announce":
+							webhookClient.send(args.join(" "));
+							break;
+						default:
+							console.log("command received, could not parse command");
+							return;
+					}
+				}
+			} else {
+				message.reply(
+					"can't follow through homie, didn't catch that last part"
+				);
+			}
+		}
+	}
 });
 
 // reaction event listener
 // ADD role
-client.on('messageReactionAdd', (reaction, user) => {
-    const { name } = reaction.emoji;
-    const member = reaction.message.guild.members.cache.get(user.id);
+client.on("messageReactionAdd", (reaction, user) => {
+	const { name } = reaction.emoji;
+	const member = reaction.message.guild.members.cache.get(user.id);
 
-    if (reaction.message.id === '749711465665003732') {
-        switch (name) {
-            //golang 749704008876294229
-            case '🐉':
-                member.roles.add('749704008876294229');
-                console.log('Added Go role to', user.id);
-                break;
-            //javascript 749704270927888425
-            case '🍌':
-                member.roles.add('749704270927888425');
-                console.log('Added JS role to', user.id);
-                break;
-            default:
-                break;
-        }
-    }
+	if (reaction.message.id === "749711465665003732") {
+		switch (name) {
+			//golang 749704008876294229
+			case "🐉":
+				member.roles.add("749704008876294229");
+				console.log("Added Go role to", user.id);
+				break;
+			//javascript 749704270927888425
+			case "🍌":
+				member.roles.add("749704270927888425");
+				console.log("Added JS role to", user.id);
+				break;
+			default:
+				break;
+		}
+	}
 });
 
 // reaction event listener
 // REMOVE role
-client.on('messageReactionRemove', (reaction, user) => {
-    const { name } = reaction.emoji;
-    const member = reaction.message.guild.members.cache.get(user.id);
+client.on("messageReactionRemove", (reaction, user) => {
+	const { name } = reaction.emoji;
+	const member = reaction.message.guild.members.cache.get(user.id);
 
-    if (reaction.message.id === '749711465665003732') {
-        switch (name) {
-            //golang 749704008876294229
-            case '🐉':
-                member.roles.remove('749704008876294229');
-                console.log('Removed Go role from', user.id);
-                break;
-            //javascript 749704270927888425
-            case '🍌':
-                member.roles.remove('749704270927888425');
-                console.log('Removed JS role from', user.id);
-                break;
-            default:
-                break;
-        }
-    }
+	if (reaction.message.id === "749711465665003732") {
+		switch (name) {
+			//golang 749704008876294229
+			case "🐉":
+				member.roles.remove("749704008876294229");
+				console.log("Removed Go role from", user.id);
+				break;
+			//javascript 749704270927888425
+			case "🍌":
+				member.roles.remove("749704270927888425");
+				console.log("Removed JS role from", user.id);
+				break;
+			default:
+				break;
+		}
+	}
 });
--- src/util/helper.js
+++ src/util/helper.js
@@ -1,56 +1,62 @@
-const Greetings = [
-    "g'day",
-    'hello',
-    'hi',
-    'hey',
-    'hiya',
-    'howdy',
-    'sup',
-    'wassup',
-    'yo',
-];
-
-const Commands = ['kick', 'ban', 'say', 'announce'];
-
-const Functions = {
-    // checks if the user is kickable and
-    // kick them async
-    checkKickable: (message, member) => {
-        if (member) {
-            member
-                .kick()
-                .then((member) =>
-                    message.channel.send(`kicked ${member}. Adios!`)
-                )
-                .catch((err) => {
-                    message.reply(
-                        "I don't have authority to do that. Their power level is over 9000!"
-                    );
-                    throw err;
-                });
-        } else {
-            message.reply("couldn't find that user");
-        }
-    },
-    // checks if the user is bannable and
-    // ban them async
-    checkBannable: (message, member) => {
-        if (member) {
-            member
-                .ban()
-                .then((member) =>
-                    message.channel.send(`banned ${member}. good riddance`)
-                )
-                .catch((err) => {
-                    message.reply(
-                        "I don't have authority to do that. Their power level is over 9000!"
-                    );
-                    throw err;
-                });
-        } else {
-            message.reply("couldn't find that user");
-        }
-    },
+const Constants = {
+	env: {
+		BOT_TOKEN: process.env.BOT_TOKEN,
+		GITHUB_WEBHOOK_ID: process.env.GITHUB_WEBHOOK_ID,
+		GITHUB_WEBHOOK_TOKEN: process.env.GITHUB_WEBHOOK_TOKEN,
+	},
 };
 
-module.exports = { Greetings, Commands, Functions };
+const Greetings = [
+	"g'day",
+	"hello",
+	"hi",
+	"hey",
+	"hiya",
+	"howdy",
+	"sup",
+	"wassup",
+	"yo",
+];
+
+const Commands = ["kick", "ban", "say", "announce"];
+
+const Functions = {
+	// checks if the user is kickable and
+	// kick them async
+	checkKickable: (message, member) => {
+		if (member) {
+			member
+				.kick()
+				.then((member) => message.channel.send(`kicked ${member}. Adios!`))
+				.catch((err) => {
+					message.reply(
+						"I don't have authority to do that. Their power level is over 9000!"
+					);
+					throw err;
+				});
+		} else {
+			message.reply("couldn't find that user");
+		}
+	},
+	// checks if the user is bannable and
+	// ban them async
+	checkBannable: (message, member) => {
+		if (member) {
+			member
+				.ban()
+				.then((member) =>
+					message.channel.send(`banned ${member}. good riddance`)
+				)
+				.catch((err) => {
+					message.reply(
+						"I don't have authority to do that. Their power level is over 9000!"
+					);
+					throw err;
+				});
+		} else {
+			message.reply("couldn't find that user");
+		}
+	},
+};
+
+module.exports = { Greetings, Commands, Functions, Constants };
